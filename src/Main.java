add189a35356fdb35e2dbabd22a5236d4bfa3924
mseglianik <mykola.s@somplo.com>
2024-11-28 23:09:25 +0200

    changed greeting and multiply func

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello this my Lab8!");
+        System.out.printf("Hello this my and my friend Lab8!");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
-        int number = multiplyTwoNumbers(2, 5);
+        int number = multiplyThreeNumbers(2, 5, 2);
         System.out.println("Multiply result: " + number);
     }
 
-    public static int multiplyTwoNumbers(int n1, int n2){
-        return n1 * n2;
+    public static int multiplyThreeNumbers(int n1, int n2, int n3){
+        return n1 * n2 * n3;
     }
 }
